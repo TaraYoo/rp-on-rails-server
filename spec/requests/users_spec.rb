90b36986969fd443dfd40d419797ac5db0b2b5e9
TaraYoo <yootar@gmail.com>
2019-03-09 13:11:39 -0500

    Get rid of unnecessary spec tests
    User get by id, and index are unnecessary features. Get rid of related
    unit tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -130,49 +130,5 @@ RSpec.describe 'Users API' do
       @token = JSON.parse(response.body)['user']['token']
       @user_id = JSON.parse(response.body)['user']['id']
     end
-
-    describe 'GET /users' do
-      it 'is successful' do
-        get '/users', headers: headers
-
-        expect(response).to be_success
-
-        parsed_response = JSON.parse(response.body)
-        expect(
-          parsed_response['users']
-        ).not_to be_empty
-      end
-    end
-
-    describe 'GET /users/:id' do
-      it 'is successful' do
-        get "/users/#{@user_id}", headers: headers
-
-        expect(response).to be_success
-
-        parsed_response = JSON.parse(response.body)
-        expect(
-          parsed_response['user']
-        ).not_to be_empty
-      end
-    end
-  end
-
-  context 'when not authenticated' do
-    describe 'GET /users' do
-      it 'is not successful' do
-        get '/users'
-
-        expect(response).not_to be_success
-      end
-    end
-
-    describe 'GET /users/:id' do
-      it 'is not successful' do
-        get "/users/#{@user_id}"
-
-        expect(response).not_to be_success
-      end
-    end
   end
 end
